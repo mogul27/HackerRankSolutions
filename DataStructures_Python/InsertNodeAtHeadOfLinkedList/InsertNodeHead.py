fe674db3ff178bc7f675009cfd9def0a3a7fd946
Mogul <mogul27442@gmail.com>
2022-08-31 13:39:23 +0100

    LinkedListHead

---
+++ DataStructures_Python/InsertNodeAtHeadOfLinkedList/InsertNodeHead.py
@@ -0,0 +1,21 @@
+
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+# The head is the back of the lisr
+def insertNodeAtHead(llist, data):
+    node = SinglyLinkedListNode(data)
+    node.next = llist
+    llist_head = node
+    
+    # llist.head is what is passed in
+    return llist_head
+
